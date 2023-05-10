48df912457a3c937bddd419c43f0369e9ed43fc7
SvjetlanaR <svjetlanarados@gmail.com>
2023-05-10 18:41:14 +0200

    changed db

--- routes/result.js
+++ routes/result.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const fightResultRouter = express.Router()
-const {getResults, createResult, deleteResult} = require('../controller/result')
+const express = require("express");
+const fightResultRouter = express.Router();
+const {
+  getResults,
+  createResult,
+  deleteResult,
+} = require("../controller/result");
 
-fightResultRouter.get('/', getResults)
-fightResultRouter.post('/', createResult)
-fightResultRouter.delete('/:id', deleteResult)
-
-
+fightResultRouter.get("/", getResults);
+fightResultRouter.post("/", createResult);
+fightResultRouter.delete("/:id", deleteResult);
 
 module.exports = {
-    fightResultRouter
-}
+  fightResultRouter,
+};
