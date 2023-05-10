63709f086e6c46a8f42ad4eab6146bca5ac125d7
SvjetlanaR <svjetlanarados@gmail.com>
2023-05-10 23:12:43 +0200

    changes

--- index.js
+++ index.js
@@ -10,22 +10,25 @@ const { fightResultRouter } = require("./routes/result");
 
 app.use(express.json());
 // app.use(cors())
-app.use(
-  cors({
-    origin: "https://pokefight-j9bg.onrender.com",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
-const port = 8082;
-
+// app.use(
+//   cors({
+//     origin: "https://pokefight-j9bg.onrender.com",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   })
+// );
+//const port = 8082;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.use("/pokemon", pokemonRouter);
 app.use("/fightresult", fightResultRouter);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
